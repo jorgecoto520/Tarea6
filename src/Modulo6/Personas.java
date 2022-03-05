942db17d8487d0b883c00b16a23345c0443ac690
Jorge Soto <100743567+jorgecoto520@users.noreply.github.com>
2022-03-04 21:00:39 -0600

    Clase Personas, constructores, get y set

--- src/Modulo6/Personas.java
+++ src/Modulo6/Personas.java
@@ -11,4 +11,71 @@ package Modulo6;
  */
 public class Personas {
     
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String profesion;
+    private  String Direccion;
+    private String pais;
+
+    public Personas(String nombre, String apellido, int edad, String profesion, String Direccion, String pais) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.profesion = profesion;
+        this.Direccion = Direccion;
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+    
+    
+    
 }
