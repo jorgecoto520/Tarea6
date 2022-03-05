ba0a1f9eadd76ae4415824a2df1221f3bca8245d
Jorge Soto <100743567+jorgecoto520@users.noreply.github.com>
2022-03-04 21:19:58 -0600

    Herencia clase doctor y metodo set encapsulado

--- src/Modulo6/Doctor.java
+++ src/Modulo6/Doctor.java
@@ -9,6 +9,25 @@ package Modulo6;
  *
  * @author HACK
  */
-public class Doctor {
+public class Doctor extends Personas{
+
+    public Doctor(String nombre, String apellido, int edad, String profesion, String Direccion, String pais) {
+        super(nombre, apellido, edad, profesion, Direccion, pais);
+    }
+    
+    public void Doctor(){
+        
+        setNombre("Juan Rodriguez");
+        setApellido("Martinez");
+        setEdad(23);
+        setDireccion("Colonia Arturo Quezada");
+        setProfesion("Doctor en medicina general");
+        setPais("Honduras");
+        
+    
+    
+    
+    }
+    
     
 }
