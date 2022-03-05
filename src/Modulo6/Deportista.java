ee2d26d73b5065e5644e5bd91772feb7cad9d211
Jorge Soto <100743567+jorgecoto520@users.noreply.github.com>
2022-03-04 21:23:30 -0600

    Herencia clase doctor y metodo set encapsulado clase deportista

--- src/Modulo6/Deportista.java
+++ src/Modulo6/Deportista.java
@@ -9,6 +9,23 @@ package Modulo6;
  *
  * @author HACK
  */
-public class Deportista {
+public class Deportista extends Personas {
+
+    public Deportista(String nombre, String apellido, int edad, String profesion, String Direccion, String pais) {
+        super(nombre, apellido, edad, profesion, Direccion, pais);
+    }
     
+    
+    public void Deportista(){
+    
+        
+        setNombre("leonel Andres");
+        setApellido(" Messi ");
+        setEdad(35);
+        setDireccion("ESPA'A BARCELONA");
+        setProfesion("Jugador del FB Barcelona");
+        setPais("Spain");
+    
+    
+    }
 }
