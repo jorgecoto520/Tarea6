773b0b0e2c6b6441c1db697a23223b32071ba63a
Jorge Soto <100743567+jorgecoto520@users.noreply.github.com>
2022-03-04 20:43:28 -0600

    Bienvenida

--- src/Modulo6/main.java
+++ src/Modulo6/main.java
@@ -4,4 +4,10 @@ package Modulo6;
 
 public class main {
     
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Bienvenidos A mi Repositorio ");
+    }
+    
 }
